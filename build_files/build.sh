dfba7a53a7c1d6c131653e9ac1c37935f4b622a3
n3ddu8 <13167147+n3ddu8@users.noreply.github.com>
2025-11-12 16:07:05 +0000

    feat: added DevPod CLI

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,4 +9,9 @@ dnf5 -y install \
         containerd \
         docker-compose
 
+# Devpod CLI for ARM
+curl -L -o devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-arm64" \
+  && install -c -m 0755 devpod /usr/local/bin \
+  && rm -f devpod
+
 systemctl enable podman.socket
