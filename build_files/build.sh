a3b496c7984a7c9360f32cc6472481eb925dfaa0
n3ddu8 <13167147+n3ddu8@users.noreply.github.com>
2025-11-12 15:35:18 +0000

    feat: added some basic packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,6 +3,9 @@
 set -ouex pipefail
 
 ### Install packages
+dnf -y install \
+	chezmoi \
+	python-pip
 
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
@@ -10,7 +13,6 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
 
 # Use a COPR Example:
 #
