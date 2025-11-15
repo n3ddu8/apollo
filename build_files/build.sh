be4623cf09f0654a1c3df39bd908e25443e7cf26
n3ddu8 <13167147+n3ddu8@users.noreply.github.com>
2025-11-15 13:49:15 +0000

    feat: added update-services

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,7 +15,9 @@ curl -L -o devpod "https://github.com/loft-sh/devpod/releases/latest/download/de
   && rm -f devpod
 
 dnf5 -y copr enable ublue-os/packages
-dnf5 -y install ublue-brew
+dnf5 -y install \
+        ublue-brew \
+        ublue-os-update-services
 dnf5 -y copr disable ublue-os/packages
 
 systemctl enable podman.socket
